4a702e369127906ec6a7560ab62757671e18ec68
Ross Clay <clay.rossclay@gmail.com>
2024-03-13 13:02:10 -0400

    creates basic calculator functions

---
+++ script.js
@@ -0,0 +1,34 @@
+function add(a, b) {
+    return a + b
+}
+
+function subtract(a, b) {
+    return a - b
+}
+
+function multiply(a, b) {
+    return a * b
+}
+
+function divide(a, b) {
+    return a / b
+}
+
+function operate(a, b, operator) {
+    switch (operator) {
+        case '+':
+            add(a, b);
+            break;
+        case '-':
+            subtract(a, b);
+            break;
+        case '*':
+            multiply(a, b);
+            break;
+        case '/':
+            divide(a, b);
+            break;
+
+    }
+
+}
