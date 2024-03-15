7a070d54dbe64964d2f6f1e2903f020f0c0a5b67
Ross Clay <clay.rossclay@gmail.com>
2024-03-15 17:04:45 -0400

    adds decimal functionality

--- script.js
+++ script.js
@@ -20,16 +20,16 @@ function divide(a, b) {
 function operate(a, b, operator) {
     switch (operator) {
         case '+':
-            add(a, b);
+            return add(a, b);
             break;
         case '-':
-            subtract(a, b);
+            return subtract(a, b);
             break;
         case '*':
-            multiply(a, b);
+            return multiply(a, b);
             break;
-        case '/':
-            divide(a, b);
+        case '÷':
+            return divide(a, b);
             break;
 
     }
@@ -38,25 +38,62 @@ function operate(a, b, operator) {
 // make the buttons work
 let calcDisplay = document.querySelector('.calc-display')
 let calcDisplayValue = calcDisplay.textContent
-
-
+let calcDisplayA = ''
+let calcDisplayB = ''
+let calcOperator = ''
+let calcDecimal = ''
 
 let numBtns = document.querySelectorAll('.num-btn')
 numBtns.forEach((numBtn) => numBtn.addEventListener('click', () => {
-    calcDisplay.textContent += numBtn.id
+    if (calcOperator === '') {
+        calcDisplayA += numBtn.id
+        calcDisplay.textContent = calcDisplayA
+    }
+    else {
+        calcDisplay
+        calcDisplayB += numBtn.id
+        calcDisplay.textContent = calcDisplayB
+    }
 }))
-
+// under construction
 let operatorBtn = document.querySelector('.operator-btn')
 operatorBtn.addEventListener('click', () => {
-    calcDisplay.textContent += operatorBtn.id
+    if (calcOperator === '') {
+        calcOperator += operatorBtn.id
+    }
+    else {
+        operate(calcDisplayA, calcDisplayB, calcOperator)
+    }
 })
-
+// under construction
 let equalBtn = document.querySelector('.equal-btn')
 equalBtn.addEventListener('click', () => {
+    operate(calcDisplayA, calcDisplayB, calcOperator)
+
+})
+// under construction
+let deleteBtn = document.querySelector('.delete-btn')
+deleteBtn.addEventListener('click', () => {
+
+    calcDisplayValue = calcDisplay.textContent.substring(0, calcDisplayValue.length - 1)
+    calcDisplay.textContent = calcDisplayValue
 
 })
 
 let clearBtn = document.querySelector('.clear-btn')
 clearBtn.addEventListener('click', () => {
     calcDisplay.textContent = 0
+    calcDisplayValue = 0
+    calcDisplayA = 0
+    calcDisplayB = 0
+})
+
+// under construction
+let decimalBtn = document.querySelector('.decimal-btn')
+decimalBtn.addEventListener('click', () => {
+    if (calcDecimal === '') {
+        calcDecimal += decimalBtn.id
+        calcDisplayA += decimalBtn.id
+        calcDisplay.textContent += decimalBtn.id
+    }
 })
