32ff6f8df28541fd76ac63902ea097b21d76d93d
Ross Clay <clay.rossclay@gmail.com>
2024-03-15 08:20:07 -0400

    adds ability to change display and stylizes UI

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+// calculator functions
+
 function add(a, b) {
     return a + b
 }
@@ -32,3 +35,28 @@ function operate(a, b, operator) {
     }
 
 }
+// make the buttons work
+let calcDisplay = document.querySelector('.calc-display')
+let calcDisplayValue = calcDisplay.textContent
+
+
+
+let numBtns = document.querySelectorAll('.num-btn')
+numBtns.forEach((numBtn) => numBtn.addEventListener('click', () => {
+    calcDisplay.textContent += numBtn.id
+}))
+
+let operatorBtn = document.querySelector('.operator-btn')
+operatorBtn.addEventListener('click', () => {
+    calcDisplay.textContent += operatorBtn.id
+})
+
+let equalBtn = document.querySelector('.equal-btn')
+equalBtn.addEventListener('click', () => {
+
+})
+
+let clearBtn = document.querySelector('.clear-btn')
+clearBtn.addEventListener('click', () => {
+    calcDisplay.textContent = 0
+})
