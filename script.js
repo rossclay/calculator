017c4ccc7ab6dfd3978af3b46d7aba6beb2987c6
Ross Clay <clay.rossclay@gmail.com>
2024-03-17 21:11:27 -0400

    adds basic calculator functionality

--- script.js
+++ script.js
@@ -31,17 +31,25 @@ function operate(a, b, operator) {
         case '÷':
             return divide(a, b);
             break;
-
     }
-
 }
-// make the buttons work
+
 let calcDisplay = document.querySelector('.calc-display')
-let calcDisplayValue = calcDisplay.textContent
+// before an operator is chosen
 let calcDisplayA = ''
+// after an operator has been chosen
 let calcDisplayB = ''
+// has an operator been chosen?
 let calcOperator = ''
+// is there a decimal? 
 let calcDecimal = ''
+
+function operateCalculator() {
+    calcDisplay.textContent = operate(parseFloat(calcDisplayA), parseFloat(calcDisplayB), calcOperator)
+    calcDisplayA = calcDisplay.textContent
+    calcDisplayB = ''
+    calcOperator = ''
+}
 
 let numBtns = document.querySelectorAll('.num-btn')
 numBtns.forEach((numBtn) => numBtn.addEventListener('click', () => {
@@ -49,43 +57,45 @@ numBtns.forEach((numBtn) => numBtn.addEventListener('click', () => {
         calcDisplayA += numBtn.id
         calcDisplay.textContent = calcDisplayA
     }
+    // once an operator has been chosen, now we want to observe the second number
     else {
-        calcDisplay
         calcDisplayB += numBtn.id
         calcDisplay.textContent = calcDisplayB
     }
 }))
 // under construction
-let operatorBtn = document.querySelector('.operator-btn')
-operatorBtn.addEventListener('click', () => {
+let operatorBtns = document.querySelectorAll('.operator-btn')
+operatorBtns.forEach((operatorBtn) => operatorBtn.addEventListener('click', () => {
     if (calcOperator === '') {
-        calcOperator += operatorBtn.id
+        calcOperator = operatorBtn.id
     }
     else {
-        operate(calcDisplayA, calcDisplayB, calcOperator)
+        operateCalculator()
     }
-})
+}))
 // under construction
 let equalBtn = document.querySelector('.equal-btn')
 equalBtn.addEventListener('click', () => {
-    operate(calcDisplayA, calcDisplayB, calcOperator)
-
+    if (calcOperator != '') {
+        operateCalculator()
+    }
 })
-// under construction
+// REALLY under construction
 let deleteBtn = document.querySelector('.delete-btn')
 deleteBtn.addEventListener('click', () => {
+    let calcDisplayValue = calcDisplay.textContent
+    calcDisplay.textContent.substring(0, calcDisplayValue.length - 1)
 
-    calcDisplayValue = calcDisplay.textContent.substring(0, calcDisplayValue.length - 1)
-    calcDisplay.textContent = calcDisplayValue
 
 })
 
 let clearBtn = document.querySelector('.clear-btn')
 clearBtn.addEventListener('click', () => {
     calcDisplay.textContent = 0
-    calcDisplayValue = 0
-    calcDisplayA = 0
-    calcDisplayB = 0
+    calcDisplayA = ''
+    calcDisplayB = ''
+    calcDecimal = ''
+    calcOperator = ''
 })
 
 // under construction
@@ -93,7 +103,14 @@ let decimalBtn = document.querySelector('.decimal-btn')
 decimalBtn.addEventListener('click', () => {
     if (calcDecimal === '') {
         calcDecimal += decimalBtn.id
-        calcDisplayA += decimalBtn.id
-        calcDisplay.textContent += decimalBtn.id
+        if (calcOperator = '') {
+            calcDisplayA += decimalBtn.id
+            calcDisplay.textContent = calcDisplayA
+        }
+        else {
+            calcDisplayB += decimalBtn.id
+            calcDisplay.textContent = calcDisplayB
+
+        }
     }
 })
