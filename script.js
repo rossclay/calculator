53dd8dae689bbb6a7e933ae2ab26f057e63c70df
Ross Clay <clay.rossclay@gmail.com>
2024-03-18 15:37:50 -0400

    improves decimal and operator functionality

--- script.js
+++ script.js
@@ -14,7 +14,12 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-    return a / b
+    if (b === 0) {
+        return 'ERROR'
+    }
+    else {
+        return a / b
+    }
 }
 
 function operate(a, b, operator) {
@@ -33,7 +38,7 @@ function operate(a, b, operator) {
             break;
     }
 }
-
+// calculator's screen
 let calcDisplay = document.querySelector('.calc-display')
 // before an operator is chosen
 let calcDisplayA = ''
@@ -41,46 +46,55 @@ let calcDisplayA = ''
 let calcDisplayB = ''
 // has an operator been chosen?
 let calcOperator = ''
-// is there a decimal? 
-let calcDecimal = ''
+// is there a decimal in the first number? 
+let calcDecimalA = ''
+// is there a decimal in the second number?
+let calcDecimalB = ''
 
 function operateCalculator() {
     calcDisplay.textContent = operate(parseFloat(calcDisplayA), parseFloat(calcDisplayB), calcOperator)
     calcDisplayA = calcDisplay.textContent
     calcDisplayB = ''
+    if (calcDisplay.textContent.includes('.')) { calcDecimalA = '.' }
+    else { calcDecimalA = '' }
+    calcDecimalB = ''
     calcOperator = ''
+    operatorBtns.forEach((operatorBtn) => operatorBtn.setAttribute('style', 'background-color: lightgray'))
 }
-
 let numBtns = document.querySelectorAll('.num-btn')
 numBtns.forEach((numBtn) => numBtn.addEventListener('click', () => {
     if (calcOperator === '') {
         calcDisplayA += numBtn.id
         calcDisplay.textContent = calcDisplayA
     }
-    // once an operator has been chosen, now we want to observe the second number
+    // after an operator has been chosen
     else {
         calcDisplayB += numBtn.id
         calcDisplay.textContent = calcDisplayB
     }
 }))
-// under construction
+
 let operatorBtns = document.querySelectorAll('.operator-btn')
 operatorBtns.forEach((operatorBtn) => operatorBtn.addEventListener('click', () => {
+
     if (calcOperator === '') {
         calcOperator = operatorBtn.id
+        operatorBtn.setAttribute('style', 'background-color: #8D8D91')
     }
     else {
         operateCalculator()
+        calcOperator = operatorBtn.id
+        operatorBtn.setAttribute('style', 'background-color: #8D8D91')
     }
 }))
-// under construction
+
 let equalBtn = document.querySelector('.equal-btn')
 equalBtn.addEventListener('click', () => {
     if (calcOperator != '') {
         operateCalculator()
     }
 })
-// REALLY under construction
+// under construction
 let deleteBtn = document.querySelector('.delete-btn')
 deleteBtn.addEventListener('click', () => {
     let calcDisplayValue = calcDisplay.textContent
@@ -94,23 +108,31 @@ clearBtn.addEventListener('click', () => {
     calcDisplay.textContent = 0
     calcDisplayA = ''
     calcDisplayB = ''
-    calcDecimal = ''
+    calcDecimalA = ''
+    calcDecimalB = ''
     calcOperator = ''
+    operatorBtns.forEach((operatorBtn) => operatorBtn.setAttribute('style', 'background-color: lightgray'))
 })
 
-// under construction
 let decimalBtn = document.querySelector('.decimal-btn')
 decimalBtn.addEventListener('click', () => {
-    if (calcDecimal === '') {
-        calcDecimal += decimalBtn.id
-        if (calcOperator = '') {
+    // does the first number already have a decimal?
+    if (calcDecimalA === '') {
+        calcDecimalA += decimalBtn.id
+        if (calcOperator === '') {
             calcDisplayA += decimalBtn.id
             calcDisplay.textContent = calcDisplayA
         }
-        else {
+    }
+    // if the first number already has a decimal, then we check to see if the second number has a decimal,
+    // AND we've started to input a number for the second value
+    else if (calcDecimalA === '.') {
+        if (calcDecimalB === '' && calcDisplayB != '') {
+            calcDecimalB += decimalBtn.id
             calcDisplayB += decimalBtn.id
             calcDisplay.textContent = calcDisplayB
-
         }
     }
+
 })
+
